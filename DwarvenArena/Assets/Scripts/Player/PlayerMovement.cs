c4a63438512421c1cb1a0a5f04a8e87ccffd8c20
Zawisz <zawisza_mateusz@wp.pl>
2021-03-06 00:00:36 +0100

    Basic Player Movement

--- DwarvenArena/Assets/Scripts/Player/PlayerMovement.cs
+++ DwarvenArena/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Parameters:")]
     [SerializeField] private float movementSpeed = 5.0f;
+    [SerializeField] private float rotationSpeed = 5.0f;
 
     [Header("References:")]
     public Animator animator = null;
@@ -19,15 +20,72 @@ public class PlayerMovement : MonoBehaviour
 
     public void Move(float movementLR, float movementFB, bool isMoving)
     {
-        bool movementValue = movementFB != 0 || movementLR != 0;
+        float movementValue = Mathf.Abs(movementFB) > Mathf.Abs(movementLR) ? movementFB : movementLR;
 
         animator?.SetBool("isMoving", isMoving);
 
-        Vector3 movement = Vector3.zero;
+        //Vector3 movement = Vector3.zero;
 
-        movement += Vector3.right * movementLR;
-        movement += Vector3.forward * movementFB;
+        //movement += Vector3.right * movementLR;
+        //movement += Vector3.forward * movementFB;
 
-        characterController?.Move(movement * movementSpeed * Time.deltaTime);
+        if (isMoving)
+            RotatePlayer(movementLR, movementFB);
+
+        characterController?.Move(transform.forward * movementSpeed * Time.deltaTime * Mathf.Abs(movementValue));
+    }
+
+    private void RotatePlayer(float movementLR, float movementFB)
+    {
+        Vector3 rotation = this.transform.rotation.eulerAngles;
+        float desirableRotation = GetDesirableRotation(movementLR, movementFB);
+
+        Vector3 desirableRotationVector = new Vector3(0, desirableRotation, 0);
+
+        Quaternion angle = Quaternion.Lerp(Quaternion.Euler(rotation.x, rotation.y, rotation.z), Quaternion.Normalize(Quaternion.Euler(desirableRotationVector.x, desirableRotationVector.y, desirableRotationVector.z)), Time.deltaTime * rotationSpeed);
+
+        this.transform.rotation = angle;
+    }
+
+    private float GetDesirableRotation(float movementLR, float movementFB)
+    {
+        float desirableRotation = 0.0f;
+
+        if (movementFB > 0.7f)
+        {
+            if (movementLR > 0.7f)
+            {
+                desirableRotation = 45.0f;
+            }
+            else if (movementLR < -0.7f)
+            {
+                desirableRotation = 315.0f;
+            }
+            else
+            {
+                desirableRotation = 0.0f;
+            }
+        }
+        else if (movementFB < -0.7f)
+        {
+            if (movementLR > 0.7f)
+            {
+                desirableRotation = 135.0f;
+            }
+            else if (movementLR < -0.7f)
+            {
+                desirableRotation = 225.0f;
+            }
+            else
+            {
+                desirableRotation = 180.0f;
+            }
+        }
+        else if (movementLR > 0.7f)
+            desirableRotation = 90.0f;
+        else if (movementLR < -0.7f)
+            desirableRotation = 270.0f;
+
+        return desirableRotation;
     }
 }
