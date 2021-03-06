dd91a6e9a94b5e0740fd701e031d13e966a19362
Zawisz <zawisza_mateusz@wp.pl>
2021-03-06 11:13:03 +0100

    Fireball

---
+++ DwarvenArena/Assets/Scripts/Weapons/Spells/CastedSpell.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CastedSpell : MonoBehaviour
+{
+    public abstract void Initialize(Vector3 source, Vector3 target);
+}
