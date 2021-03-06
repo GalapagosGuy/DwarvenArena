3425fbe50ba9b967f6f4f7fd862011672cd8b202
Quivscor <quivscor@gmail.com>
2021-03-06 02:38:42 +0100

    Context behaviour start

---
+++ DwarvenArena/Assets/Scripts/VectorCast.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorCast
+{
+    /// <summary>
+    /// Casts Vec3 to Vec2, discarding Y
+    /// </summary>
+    /// <returns></returns>
+    public static Vector2 CastVector3ToVector2(Vector3 vector)
+    {
+        return new Vector2(vector.x, vector.z);
+    }
+}
