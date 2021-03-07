5d7d85d81334a5477d557240a907e9dd21b5cd6e
Zawisz <zawisza_mateusz@wp.pl>
2021-03-07 04:54:53 +0100

    Blizzard spell

---
+++ DwarvenArena/Assets/Scripts/IceFractured.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceFractured : MonoBehaviour
+{
+    private void Start()
+    {
+        MeshRenderer[] mrs = GetComponentsInChildren<MeshRenderer>();
+
+        foreach (MeshRenderer mr in mrs)
+        {
+            mr.material.SetFloat("Display", -1.0f);
+        }
+    }
+}
