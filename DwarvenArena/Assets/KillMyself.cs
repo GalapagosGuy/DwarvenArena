adc4cc57576ef4f94b6bf890749323abfa36a3c7
Quivscor <quivscor@gmail.com>
2021-03-07 11:55:58 +0100

    Tutorial start

---
+++ DwarvenArena/Assets/KillMyself.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillMyself : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Destroy(this, .1f);
+    }
+}
---
+++ DwarvenArena/Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialManager : MonoBehaviour
+{
+    public static TutorialManager Instance;
+    public List<GameObject> tutorialPopups;
+    public int nextPopupIndex = 0;
+
+    private EnemySpawner spawnerRef;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(this.gameObject);
+    }
+
+    private void Start()
+    {
+        spawnerRef = EnemySpawner.Instance;
+        SpawnTextWindow();
+    }
+
+    public void SpawnTextWindow()
+    {
+        tutorialPopups[nextPopupIndex].SetActive(true);
+        nextPopupIndex++;
+    }
+
+    public void ContinueTutorial()
+    {
+        tutorialPopups[nextPopupIndex - 1].SetActive(false);
+    }
+}
