4636bac5a7fb112ca7dc1675357d8a924445554c
B.Krukowski <38346579+OrangeInside@users.noreply.github.com>
2021-03-09 21:44:02 +0100

    Timer

---
+++ DwarvenArena/Assets/Timer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    private float time = 0;
+    private TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+
+         time += Time.deltaTime;
+
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        string _text = "";
+        int minutes = (int)time / 60;
+        int seconds = (int)time % 60;
+        if (minutes < 10)
+        {
+            _text = "0";
+        }
+        _text += minutes.ToString() + ":";
+        if (seconds < 10)
+        {
+            _text += "0";
+        }
+        _text += seconds.ToString();
+        text.text = _text;
+    }
+
+}
